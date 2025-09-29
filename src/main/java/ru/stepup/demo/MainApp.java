17744c50f88b6107dea8e54d8b09599b947b5310
Oleg Stepanyuk <qa.stepanyuk@yandex.ru>
2025-09-29 22:47:27 +0300

    Comment

--- src/main/java/ru/stepup/demo/MainApp.java
+++ src/main/java/ru/stepup/demo/MainApp.java
@@ -1,10 +1,7 @@
 package ru.stepup.demo;
-
-import java.util.Arrays;
-
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World2!");
+        System.out.println("Hello World3!");
     }
 }
