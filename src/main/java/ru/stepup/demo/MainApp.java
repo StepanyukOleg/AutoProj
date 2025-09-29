64726ea17491f750fd179056459737217c4b41e7
Oleg Stepanyuk <qa.stepanyuk@yandex.ru>
2025-09-29 23:42:27 +0300

    NewBranch

--- src/main/java/ru/stepup/demo/MainApp.java
+++ src/main/java/ru/stepup/demo/MainApp.java
@@ -2,6 +2,6 @@ package ru.stepup.demo;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello8 World5!");
+        System.out.println("Hello World1!");
     }
 }
